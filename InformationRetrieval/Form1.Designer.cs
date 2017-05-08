5aa352ecc807ff219ed0ae532bc1917eab6fb57a
Abdulla awad <abdu.awad92@outlook.com>
2017-05-08 10:00:58 +0300

    adding project files

---
+++ InformationRetrieval/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace InformationRetrieval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addFiles_button = new System.Windows.Forms.Button();
+            this.addFiles_dialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.index_button = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.delete_files_button = new System.Windows.Forms.Button();
+            this.Clear_index_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addFiles_button
+            // 
+            this.addFiles_button.Location = new System.Drawing.Point(96, 135);
+            this.addFiles_button.Name = "addFiles_button";
+            this.addFiles_button.Size = new System.Drawing.Size(139, 52);
+            this.addFiles_button.TabIndex = 0;
+            this.addFiles_button.Text = "Add New Files";
+            this.addFiles_button.UseVisualStyleBackColor = true;
+            this.addFiles_button.Click += new System.EventHandler(this.addFiles_button_Click);
+            // 
+            // addFiles_dialog
+            // 
+            this.addFiles_dialog.FileName = "addFiles_dialog";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.Location = new System.Drawing.Point(271, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "enter your search here";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // index_button
+            // 
+            this.index_button.Location = new System.Drawing.Point(96, 194);
+            this.index_button.Name = "index_button";
+            this.index_button.Size = new System.Drawing.Size(139, 52);
+            this.index_button.TabIndex = 2;
+            this.index_button.Text = "Index";
+            this.index_button.UseVisualStyleBackColor = true;
+            this.index_button.Click += new System.EventHandler(this.index_button_Click);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(368, 128);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(154, 30);
+            this.Search.TabIndex = 5;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // delete_files_button
+            // 
+            this.delete_files_button.Location = new System.Drawing.Point(96, 79);
+            this.delete_files_button.Name = "delete_files_button";
+            this.delete_files_button.Size = new System.Drawing.Size(138, 48);
+            this.delete_files_button.TabIndex = 6;
+            this.delete_files_button.Text = "Delete All Files";
+            this.delete_files_button.UseVisualStyleBackColor = true;
+            this.delete_files_button.Click += new System.EventHandler(this.delete_files_button_Click);
+            // 
+            // Clear_index_button
+            // 
+            this.Clear_index_button.Location = new System.Drawing.Point(96, 253);
+            this.Clear_index_button.Name = "Clear_index_button";
+            this.Clear_index_button.Size = new System.Drawing.Size(139, 46);
+            this.Clear_index_button.TabIndex = 7;
+            this.Clear_index_button.Text = "Clear Index";
+            this.Clear_index_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 373);
+            this.Controls.Add(this.Clear_index_button);
+            this.Controls.Add(this.delete_files_button);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.index_button);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addFiles_button);
+            this.Name = "Form1";
+            this.Text = "Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addFiles_button;
+        private System.Windows.Forms.OpenFileDialog addFiles_dialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button index_button;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button delete_files_button;
+        private System.Windows.Forms.Button Clear_index_button;
+    }
+}
+
---
+++ InformationRetrieval/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace InformationRetrieval
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            addFiles_dialog.Multiselect = true;
+            addFiles_dialog.Filter = "text files (*.txt)|*.txt|word files (*.doc,*.docx)|*.doc,*.docx|All files (*.*)|*.*";
+            cmd.Connection = conn;
+        }
+        string target = System.IO.Directory.GetCurrentDirectory() + "\\Docs";
+        Database4Entities dbe = new Database4Entities();
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abdua\Documents\Visual Studio 2015\Projects\InformationRetrieval\InformationRetrieval\bin\Debug\Database4.mdf;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        
+        
+
+        private void addFiles_button_Click(object sender, EventArgs e)
+        {
+            
+            if (addFiles_dialog.ShowDialog()==DialogResult.OK)
+            {
+                if(!System.IO.Directory.Exists(target))
+                {
+                    System.IO.Directory.CreateDirectory(target);
+                    MessageBox.Show("target directory created");
+                }
+                foreach(string s in addFiles_dialog.FileNames)
+                {
+                    bool exist = true;
+                    System.IO.File.Copy(s, target+"\\"+System.IO.Path.GetFileName(s), true);
+                    conn.Open();
+                    cmd.CommandText = "select id from Docs where link = '" + target + "\\" + System.IO.Path.GetFileName(s) + "'";
+                    try
+                    {
+                        cmd.ExecuteScalar().ToString();
+                    }
+                    catch (System.NullReferenceException)
+                    {
+                        cmd.CommandText = "insert into Docs (link) values ('" + target + "\\" + System.IO.Path.GetFileName(s) + "')";
+                        cmd.ExecuteNonQuery();
+                        exist = false;
+                    }
+                    if (exist)
+                    {
+                        MessageBox.Show("document already exists");
+                    }
+                    conn.Close();
+                }
+                MessageBox.Show("Done Adding!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string querey = textBox1.Text;
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                
+                MessageBox.Show("your search is: " + textBox1.Text);
+            }
+        }
+
+       
+
+       
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("your search is: " + textBox1.Text);
+        }
+
+        private void index_button_Click(object sender, EventArgs e)
+        {
+            createIndex();
+        }
+        void createIndex()
+        {
+            if (!System.IO.Directory.Exists(target))
+            {
+                MessageBox.Show("there is no Docs directory");
+            }
+            else if (!System.IO.Directory.GetFiles(target).Any())
+            {
+                MessageBox.Show("there is no files in Docs directory");
+            }
+            else
+                foreach(string file in System.IO.Directory.GetFiles(target))
+                {
+                    conn.Open();
+                    cmd.CommandText = "select id from Docs where link='" + file + "'";
+                    string id = cmd.ExecuteScalar().ToString();
+                    cmd.Dispose();
+                    LinkedList<string> words = new LinkedList<string>(Stopwords.RemoveStopwords(file));
+                    while (words.First != null)
+                    {
+                        cmd.CommandText = "insert into Terms (term) value ('" + words.First.ToString() + "')";
+
+                    }
+
+                    conn.Close();
+                }
+            MessageBox.Show("ok");
+        }
+
+        private void delete_files_button_Click(object sender, EventArgs e)
+        {
+            foreach(string file in System.IO.Directory.GetFiles(target))
+            {
+                System.IO.File.Delete(file);                
+                conn.Open();
+                cmd.CommandText = "delete from Docs where link='" + file + "'";
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                conn.Close();                
+            }
+            MessageBox.Show("Done Deleting");
+        }
+    }
+}
